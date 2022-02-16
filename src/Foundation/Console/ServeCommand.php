885b8d55c8c2bf3be3ae28766e9489ffe69b67e2
wanganlin <inbox@wanganlin.com>
2022-02-16 14:57:30 +0800

    update

--- src/Foundation/Console/ServeCommand.php
+++ src/Foundation/Console/ServeCommand.php
@@ -59,7 +59,6 @@ class ServeCommand extends Command
         }
 
         Config::load(config_path());
-        $config = config('server');
 
         if ($timezone = config('app.default_timezone')) {
             date_default_timezone_set($timezone);
@@ -67,18 +66,24 @@ class ServeCommand extends Command
 
         Worker::$onMasterReload = function () {
             if (function_exists('opcache_get_status')) {
-                if (isset($status['scripts']) && $scripts = $status['scripts']) {
-                    foreach (array_keys($status['scripts']) as $file) {
-                        opcache_invalidate($file, true);
+                if ($status = opcache_get_status()) {
+                    if (isset($status['scripts']) && $scripts = $status['scripts']) {
+                        foreach (array_keys($status['scripts']) as $file) {
+                            opcache_invalidate($file, true);
+                        }
                     }
                 }
             }
         };
 
+        $config = config('server');
         Worker::$pidFile = $config['pid_file'];
         Worker::$stdoutFile = $config['stdout_file'];
         Worker::$logFile = $config['log_file'];
         TcpConnection::$defaultMaxPackageSize = $config['max_package_size'] ?? 10 * 1024 * 1024;
+        if (property_exists(Worker::class, 'statusFile')) {
+            Worker::$statusFile = $config['status_file'] ?? '';
+        }
 
         $worker = new Worker($config['listen'], $config['context']);
         $property_map = [
@@ -96,99 +101,23 @@ class ServeCommand extends Command
         }
 
         $worker->onWorkerStart = function ($worker) {
-            set_error_handler(function ($level, $message, $file = '', $line = 0) {
-                if (error_reporting() & $level) {
-                    throw new ErrorException($message, 0, $level, $file, $line);
-                }
-            });
-            register_shutdown_function(function ($start_time) {
-                if (time() - $start_time <= 1) {
-                    sleep(1);
-                }
-            }, time());
-            foreach (config('autoload.files', []) as $file) {
-                include_once $file;
-            }
-            if (class_exists('Dotenv\Dotenv')) {
-                if (method_exists('Dotenv\Dotenv', 'createUnsafeMutable')) {
-                    Dotenv::createUnsafeMutable(base_path())->load();
-                } else {
-                    Dotenv::createMutable(base_path())->load();
-                }
-            }
-            Config::reload(config_path());
-            foreach (config('app.providers', []) as $class_name) {
-                /** @var \Swift\Contracts\Bootstrap $class_name */
-                $class_name::start($worker);
-            }
+            require_once dirname(__DIR__, 3) . '/Support/bootstrap.php';
             $app = new App($worker, Container::instance(), Log::channel('default'), app_path(), public_path());
-            Route::load(base_path() . '/routes/route.php');
-            Middleware::load(config('middleware', []));
-            Middleware::load(['__static__' => config('static.middleware', [])]);
             Http::requestClass(Request::class);
-
             $worker->onMessage = [$app, 'onMessage'];
         };
 
         // Windows does not support custom processes.
         if (DIRECTORY_SEPARATOR === '/') {
             foreach (config('process', []) as $process_name => $config) {
-                $worker = new Worker($config['listen'] ?? null, $config['context'] ?? []);
-                $property_map = [
-                    'count',
-                    'user',
-                    'group',
-                    'reloadable',
-                    'reusePort',
-                    'transport',
-                    'protocol',
-                ];
-                $worker->name = $process_name;
-                foreach ($property_map as $property) {
-                    if (isset($config[$property])) {
-                        $worker->$property = $config[$property];
+                worker_start($process_name, $config);
+            }
+            foreach (config('plugin', []) as $firm => $projects) {
+                foreach ($projects as $name => $project) {
+                    foreach ($project['process'] ?? [] as $process_name => $config) {
+                        worker_start("plugin.$firm.$name.$process_name", $config);
                     }
                 }
-
-                $worker->onWorkerStart = function ($worker) use ($config) {
-                    foreach (config('autoload.files', []) as $file) {
-                        include_once $file;
-                    }
-                    if (class_exists('Dotenv\Dotenv')) {
-                        Dotenv::createMutable(base_path())->load();
-                    }
-                    Config::reload(config_path());
-
-                    $bootstrap = config('app.providers', []);
-                    foreach ($bootstrap as $class_name) {
-                        /** @var \Swift\Contracts\Bootstrap $class_name */
-                        $class_name::start($worker);
-                    }
-
-                    foreach ($config['services'] ?? [] as $server) {
-                        if (!class_exists($server['handler'])) {
-                            echo "process error: class {$server['handler']} not exists\r\n";
-                            continue;
-                        }
-                        $listen = new Worker($server['listen'] ?? null, $server['context'] ?? []);
-                        if (isset($server['listen'])) {
-                            echo "listen: {$server['listen']}\n";
-                        }
-                        $instance = Container::make($server['handler'], $server['constructor'] ?? []);
-                        worker_bind($listen, $instance);
-                        $listen->listen();
-                    }
-
-                    if (isset($config['handler'])) {
-                        if (!class_exists($config['handler'])) {
-                            echo "process error: class {$config['handler']} not exists\r\n";
-                            return;
-                        }
-
-                        $instance = Container::make($config['handler'], $config['constructor'] ?? []);
-                        worker_bind($worker, $instance);
-                    }
-                };
             }
         }
 
