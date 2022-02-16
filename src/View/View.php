8feffea466678ac43b9d7d8ec3e469b9f14952c3
wanganlin <inbox@wanganlin.com>
2022-02-16 15:37:09 +0800

    update view

--- src/View/View.php
+++ src/View/View.php
@@ -2,22 +2,52 @@
 
 namespace Swift\View;
 
+use Swift\Contracts\View as ViewContract;
+
 /**
- * 模版赋值
  * Class View
  * @package Swift\View
  */
-class View
+class View implements ViewContract
 {
     /**
+     * @var array
+     */
+    protected static $_vars = [];
+
+    /**
      * @param $name
-     * @param $value
-     * @return mixed
+     * @param null $value
      */
     public static function assign($name, $value = null)
     {
-        static $handler;
-        $handler = $handler ? : config('view.handler');
-        $handler::assign($name, $value);
+        static::$_vars = array_merge(static::$_vars, is_array($name) ? $name : [$name => $value]);
+    }
+
+    /**
+     * @param $template
+     * @param $vars
+     * @param string $app
+     * @return string
+     */
+    public static function render($template, $vars, $app = null): string
+    {
+        static $views = [];
+
+        $app = is_null($app) ? request()->app : $app;
+
+        if (!isset($views[$app])) {
+            $viewPath = resource_path('views' . ($app ? DIRECTORY_SEPARATOR . $app : ''));
+            $cachePath = runtime_path('views' . ($app ? DIRECTORY_SEPARATOR . $app : ''));
+            if (!is_dir($cachePath)) {
+                mkdir($cachePath, 0755, true);
+            }
+            $views[$app] = $views[$app] ?? new Blade($viewPath, $cachePath);
+        }
+
+        $vars = array_merge(static::$_vars, $vars);
+        $content = $views[$app]->render($template, $vars);
+        static::$_vars = [];
+        return $content;
     }
 }
