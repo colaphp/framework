0cc5a643eca289edbb8fcf777f36c28f5f020dc2
wanganlin <inbox@wanganlin.com>
2022-02-16 13:09:41 +0800

    update middleware

--- src/Http/Middleware/Middleware.php
+++ src/Http/Middleware/Middleware.php
@@ -2,6 +2,8 @@
 
 namespace Swift\Http\Middleware;
 
+use Psr\Container\ContainerInterface;
+use RuntimeException;
 use Swift\Foundation\App;
 
 /**
@@ -10,6 +12,11 @@ use Swift\Foundation\App;
  */
 class Middleware
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected static $_container = null;
+
     /**
      * @var array
      */
@@ -21,9 +28,12 @@ class Middleware
     public static function load($all_middlewares)
     {
         foreach ($all_middlewares as $app_name => $middlewares) {
+            if (!is_array($middlewares)) {
+                throw new RuntimeException('Bad middleware config');
+            }
             foreach ($middlewares as $class_name) {
                 if (method_exists($class_name, 'process')) {
-                    static::$_instances[$app_name][] = [App::container()->get($class_name), 'process'];
+                    static::$_instances[$app_name][] = [static::container()->get($class_name), 'process'];
                 } else {
                     // @todo Log
                     echo "middleware $class_name::process not exsits\n";
@@ -54,5 +64,20 @@ class Middleware
     public static function hasMiddleware($app_name)
     {
         return isset(static::$_instances[$app_name]);
+    }
+
+    /**
+     * @param $container
+     * @return ContainerInterface
+     */
+    public static function container($container = null)
+    {
+        if ($container) {
+            static::$_container = $container;
+        }
+        if (!static::$_container) {
+            static::$_container = App::container();
+        }
+        return static::$_container;
     }
 }
