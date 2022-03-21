074bac69e2b24c23fe10dcc5ca9bd93174ff933d
wanganlin <inbox@wanganlin.com>
2022-03-21 09:04:22 +0800

    Update Log.php

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -67,7 +67,7 @@ class Log
         /** @var HandlerInterface $handler */
         $handler = new $class(... array_values($constructor));
 
-        if ($handler instanceof FormattableHandlerInterface && $formatterConfig != []) {
+        if ($handler instanceof FormattableHandlerInterface && $formatterConfig) {
             $formatterClass = $formatterConfig['class'];
             $formatterConstructor = $formatterConfig['constructor'];
 
